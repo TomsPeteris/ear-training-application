f2c0d151685aa995f85d4c0bb6b34e54f4cf04ad
Toms Pēteris Korsaks <tomspeteriskorsaks@Tomss-MacBook-Air.local>
2020-11-29 01:53:51 +0200

    Fix user sidebar navigation

--- resources/js/Layouts/Navigation/Sidebar.vue
+++ resources/js/Layouts/Navigation/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <aside class="z-20 hidden w-64 overflow-y-auto bg-white bg-gray-800 md:block flex-shrink-0">
+    <aside class="z-20 hidden w-64 overflow-y-auto bg-white md:block flex-shrink-0">
         <div class="py-4 text-gray-500">
             <a class="ml-6 text-lg font-bold text-gray-800" href="#">
                 Dashboard
