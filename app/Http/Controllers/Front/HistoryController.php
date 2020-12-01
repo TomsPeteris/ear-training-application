b092e7c2e98ceb8c90933066b6153123416fcf77
Toms Pēteris Korsaks <tomspeteriskorsaks@Tomss-MacBook-Air.local>
2020-12-01 14:56:54 +0200

    Update history module

--- app/Http/Controllers/Front/HistoryController.php
+++ app/Http/Controllers/Front/HistoryController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class HistoryController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Front/History/Index');
+        $exercises = auth()->user()->exercises->map(function ($exercise) {
+            return [
+                'id' => $exercise->id,
+                'type' => $exercise->type,
+                'accuracy' => $exercise->accuracy,
+                'created_at' => date('D m Y H:i', strtotime($exercise->created_at))
+            ];
+        });
+
+        return Inertia::render('Front/History/Index', [
+            'exercises' => $exercises
+        ]);
     }
 }
--- resources/js/Pages/Front/History/Index.vue
+++ resources/js/Pages/Front/History/Index.vue
@@ -1,27 +1,55 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    History index
-                </div>
-            </div>
+    <div class="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
+        <div class="w-full overflow-x-auto">
+            <table class="w-full whitespace-no-wrap">
+                <thead>
+                    <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                        <th class="px-5 py-3">Type</th>
+                        <th class="px-5 py-3">Accuracy</th>
+                        <th class="px-5 py-3" colspan="2">Date</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                    <tr v-for="exercise in exercises" class="hover:bg-gray-100 focus-within:bg-gray-100">
+                        <td class="px-4 py-3">
+                            <inertia-link :href="route('exercise.overview', exercise.id)" class="flex items-center">
+                                <p class="text-gray-900 capitalize">
+                                    {{ exercise.type }}
+                                </p>
+                            </inertia-link>
+                        </td>
+                        <td class="border-t">
+                            <inertia-link class="p-4 flex items-center text-left" :href="route('exercise.overview', exercise.id)" tabindex="-1">
+                                {{ exercise.accuracy }}%
+                            </inertia-link>
+                        </td>
+                        <td class="border-t">
+                            <inertia-link class="p-4 flex items-center" :href="route('exercise.overview', exercise.id)" tabindex="-1">
+                                {{ exercise.created_at }}
+                            </inertia-link>
+                        </td>
+                        <td class="border-t w-px">
+                            <inertia-link class="p-4 flex items-center" :href="route('exercise.overview', exercise.id)" tabindex="-1">
+                                <svg viewBox="0 0 20 20" class="h-5 w-5 text-gray-400 fill-current">
+                                    <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                                </svg>
+                            </inertia-link>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-    </app-layout>
+    </div>
 </template>
 
 <script>
     import AppLayout from '../../../Layouts/AppLayout'
 
     export default {
-        components: {
-            AppLayout,
+        layout: AppLayout,
+
+        props: {
+            exercises: Array
         },
     }
 </script>
