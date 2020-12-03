2fd34881d17120a1237662d41fdaec34af1530fa
Toms Pēteris Korsaks <tomspeteriskorsaks@Tomss-MacBook-Air.local>
2020-12-03 06:57:05 +0200

    Add chartjs to project and create custom components

---
+++ resources/js/Shared/DoughnutChart.vue
@@ -0,0 +1,16 @@
+<script>
+    import { Doughnut } from 'vue-chartjs'
+
+    export default {
+        extends: Doughnut,
+
+        props: {
+            data: Object,
+            options: Object
+        },
+
+        mounted () {
+            this.renderChart(this.data, this.options)
+        }
+    }
+</script>
---
+++ resources/js/Shared/RadarChart.vue
@@ -0,0 +1,16 @@
+<script>
+    import { Radar } from 'vue-chartjs'
+
+    export default {
+        extends: Radar,
+
+        props: {
+            data: Object,
+            options: Object
+        },
+
+        mounted () {
+            this.renderChart(this.data, this.options)
+        }
+    }
+</script>
